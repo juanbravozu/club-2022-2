6a603d49ae180dbad63ac64afa93116600705153
Juan Bravo <juanda.bravo98@gmail.com>
2022-10-21 18:28:50 -0500

    firebase/button-counter

--- firebase-login/pages/index.js
+++ firebase-login/pages/index.js
@@ -1,7 +1,64 @@
-export default function Home() {
+import { useCallback, useState } from 'react';
+import { doc, updateDoc, getDoc } from 'firebase/firestore';
+import { db } from '../utils/firebaseConfig';
+
+async function getClicksData() {
+  const userDoc = doc(db, `users/Cb6zTmh4kDqfmJURQGJB`);
+  const data = await getDoc(userDoc);
+  let {clicks} = data.data();
+
+  return [clicks, userDoc]
+}
+
+export default function Home(
+  {
+    clicks
+  }
+) {
+  const [counter, setCounter] = useState(clicks ? clicks : 0);
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = useCallback(async () => {
+    try {
+      if(loading) return;
+
+      setLoading(true);
+
+      let [clicks, userDoc] = await getClicksData();
+      
+      clicks++;
+      await updateDoc(userDoc, {clicks});
+      console.log(clicks);
+      setCounter(clicks);
+      
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, [setCounter]);
+
   return (
     <div>
-      <h2>Mi página :D</h2>
+      <button onClick={handleClick}>
+        {counter}
+      </button>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  try {
+    const [clicks] = await getClicksData();
+
+    return {
+      props: {clicks}
+    }
+  } catch(error) {
+    console.error(error.message);
+
+    return {
+      props: {error: error.message}
+    };
+  }
 }
